83bb8d66aa816b6bdaabcfb118ec885e1c7e0f49
gabrielosorioz <gabrielosorio013@gmail.com>
2024-01-21 12:56:38 -0300

    feat:(main.js): Adicionar funcionalidades interativas na seção de Carros
    - Implementar Mixitup para animações e filtragem dinâmica dos cards.
    - Adicionar funcionalidade para destacar o botão de filtro ativo.
    - Criar função para manipular eventos de clique nos botões de filtro.

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,10 +64,25 @@ var swiperPopular = new Swiper(".popular__container", {
   });
 
 /*=============== MIXITUP FILTER FEATURED ===============*/
+let mixerFeatured = mixitup('.featured__content', {
+  selectors: {
+      target: '.featured__card'
+  },
+  animation: {
+      duration: 300
+  }
+});
 
+/* Link active featured */
+const linkFeatured = document.querySelectorAll('.featured__item')
 
-/* Link active featured */ 
+function activeFeatured(){
+  linkFeatured.forEach(l=>l.classList.remove('active-featured'))
+  this.classList.add('active-featured')
 
+}
+
+linkFeatured.forEach(l=>l.addEventListener('click',activeFeatured))
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
